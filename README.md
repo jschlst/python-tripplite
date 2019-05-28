d85833e936d3f6a4c5e3a35b3d88fa5d59359225
Jay Schulist <jayschulist@gmail.com>
2019-05-27 23:43:19 -0500

    Update script to use telnet since SSH doesn't respond to commands.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # python-tripplite
-PDU control using python script
+
+Tripp Lite PDU control using a python script
+
+Written by Jay Schulist <jayschulist@gmail.com>
+Version 0.1.0
+Date 05/27/2019
+
+./python-tripplite.py -h [hostname] -u [username] -p [password] --[command]
+    -h : hostname of the PDU unit to login to
+    -u : username to use for login
+    -p : Password to use for login
+    --[command] : one of the following
+        --status
+        --cycle=[all|outlet_num]
+        --on=[all|outlet_num]
+        --off=[all|outlet_num]
+
+Example(s):
+    This will print information about the given PDU:
+        ./python-tripplite.py -h pdu-e4 -u localadmin -p localadmin --status
+
+    This will cycle all outlets on the given PDU:
+        ./python-tripplite.py -h pdu-e4 -u localadmin -p localadmin --cycle=all
+
+    This will power off all outlets on the given PDU:
+        ./python-tripplite.py -h pdu-e4 -u localadmin -p localadmin --off=all
+
+    This will power on all outlets on the given PDU:
+        ./python-tripplite.py -h pdu-e4 -u localadmin -p localadmin --on=all
+
--- python-tripplite.py
+++ python-tripplite.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 
-''' This runs a sequence of commands on a remote PDU using SSH.
+''' This runs a sequence of commands on a remote Tripp Lite PDU.
 
 Written by Jay Schulist <jayschulist@gmail.com>
-Version 0.0.9
-Date 08/05/2018
+Version 0.1.0
+Date 05/27/2019
 
-./python-tripplite.py -h [ip_address] -u [username] -p [password] --[command]
+./python-tripplite.py -h [hostname] -u [username] -p [password] --[command]
     -h : hostname of the PDU unit to login to
     -u : username to use for login
     -p : Password to use for login
     --[command] : one of the following
 	--status
 	--cycle=[all|outlet_num]
-	--power_on=[all|outlet_num]
-	--power_off=[all|outlet_num]
-	--identity
+	--on=[all|outlet_num]
+	--off=[all|outlet_num]
 
-Example:
+Example(s):
     This will print information about the given PDU:
-        ./python-tripplite.py -h 192.168.2.54 -u localadmin -p localadmin --status
+        ./python-tripplite.py -h pdu-e4 -u localadmin -p localadmin --status
+
+    This will cycle all outlets on the given PDU:
+        ./python-tripplite.py -h pdu-e4 -u localadmin -p localadmin --cycle=all
+
+    This will power off all outlets on the given PDU:
+        ./python-tripplite.py -h pdu-e4 -u localadmin -p localadmin --off=all
+
+    This will power on all outlets on the given PDU:
+        ./python-tripplite.py -h pdu-e4 -u localadmin -p localadmin --on=all
 '''
 
 from __future__ import print_function
@@ -28,7 +36,7 @@ from __future__ import absolute_import
 
 import os, sys, re, getopt, getpass
 import pexpect
-
+import time
 
 try:
     raw_input
@@ -41,19 +49,16 @@ except NameError:
 # Some constants.
 #
 COMMAND_PROMPT = '>> '
+
 # This is the prompt we get if SSH does not have the remote host's public key stored in the cache.
-SSH_NEWKEY = '(?i)are you sure you want to continue connecting'
+#SSH_NEWKEY = '(?i)are you sure you want to continue connecting'
 
 def exit_with_usage():
     print(globals()['__doc__'])
     os._exit(1)
 
-
-def send_command(child, number=None):
-    child.sendline(number)
-    child.expect(COMMAND_PROMPT)
-    print(child.before)
-
+def send_command(child, menu_num=None, prompt=COMMAND_PROMPT, quiet=False):
+        print(str(child))
 
 def main():
 
@@ -62,131 +67,171 @@ def main():
     ## Parse the options, arguments, get ready, etc.
     ######################################################################
     try:
-        optlist, args = getopt.getopt(sys.argv[1:], 'h:u:p:', ['status', 'cycle=', 'power_on=', 'power_off=', 'identity'])
+        optlist, args = getopt.getopt(sys.argv[1:], 'h:u:p:', ['status', 'cycle=', 'on=', 'off='])
     except Exception as e:
         print(str(e))
         exit_with_usage()
     options = dict(optlist)
     if len(args) > 3:
         exit_with_usage()
+    #print(options)
 
     if '-h' in options:
-        host = options['-h']
+        hostname = options['-h']
     else:
-        host = raw_input('hostname: ')
+        hostname = raw_input('hostname: ')
     if '-u' in options:
-        user = options['-u']
+        username = options['-u']
     else:
-        user = raw_input('username: ')
+        username = raw_input('username: ')
     if '-p' in options:
         password = options['-p']
     else:
         password = getpass.getpass('password: ')
 
 
-    # XXX: only takes one command per execution, the first match
+    # takes one command per execution, the first match
     # --status
     # --[cycle|power_on|power_off] [all|1-24]
     status    = False
     cycle     = None 
     power_on  = None 
     power_off = None
-    identity  = False 
     if '--status' in options:
 	status = True
     elif '--cycle' in options:
 	cycle = options['--cycle']
-    elif '--power_on' in options:
-	power_on = options['--power_on']
-    elif '--power_off' in options:
-	power_off = options['--power_off']
-    elif '--identity' in options:
-	identity = True
+    elif '--on' in options:
+	power_on = options['--on']
+    elif '--off' in options:
+	power_off = options['--off']
+    else:
+        exit_with_usage()
 
 
     #
-    # Login via SSH
+    # Login via telnet (SSH doesn't work with pexpect) 
     #
-    child = pexpect.spawn('ssh -l %s %s'%(user, host))
-    i = child.expect([pexpect.TIMEOUT, SSH_NEWKEY, COMMAND_PROMPT, '(?i)password'])
+    child = pexpect.spawn('telnet %s' % hostname)
+    child.logfile = open('tripplite.log', 'w')
+    child.expect('Username: ')
+    child.sendline('%s' % username)
+    child.expect('Password: ')
+    child.sendline('%s' % password)
+    print('telnet connected to %s@%s' % (username, hostname))
+
+    '''
+    # SSH login works, but menu commands do not
+    child = pexpect.spawn('ssh -l %s %s'%(username, hostname))
+    child.logfile = open('tripplite.log', 'w')
+    child.delaybeforesend = 3 
+    child.delayaftersend = 3
+    #child.logfile = sys.stdout
+    #child.setecho(False)
+    #child.delaybeforesend = None
+    i = child.expect([pexpect.TIMEOUT, SSH_NEWKEY, COMMAND_PROMPT, '(?i)password:'])
     if i == 0: # Timeout
         print('ERROR! could not login with SSH. Here is what SSH said:')
         print(child.before, child.after)
         print(str(child))
         sys.exit (1)
     if i == 1: # In this case SSH does not have the public key cached.
+	print('no ssh key cached')
         child.sendline ('yes')
-        child.expect ('(?i)password')
+        child.expect ('(?i)password:')
     if i == 2:
+        print('option 2')
         # This may happen if a public key was setup to automatically login.
         # But beware, the COMMAND_PROMPT at this point is very trivial and
         # could be fooled by some output in the MOTD or login message.
         pass
     if i == 3:
         child.sendline(password)
-	child.expect(COMMAND_PROMPT)
-	#print(child.before)
-
+        print('ssh connected.')
+    '''
 
     # Now we should be at the command prompt and ready to run some commands.
 
     # Devices Menu
-    send_command(child, '1')
+    child.expect(COMMAND_PROMPT)
+    child.sendline('1')
 
-    # Status Menu
+    # status
     if status:
-	print('status')
-	send_command(child, '1')
+        print('Status: %s' % hostname)
+        child.expect(COMMAND_PROMPT)
+        child.sendline('1')
+        child.expect(COMMAND_PROMPT)
+        output_status = child.before[92:-75]
+        print(output_status)
+
+    # cycle outlets
     elif cycle != None:
-	print('cycle: %s'%(cycle))
-	if cycle == 'all':
+	if 'all' in cycle:
 	    # cycle all loads
-	    send_command(child, '3')
-	    send_command(child, '6')
+            print('Cycling ALL loads on %s... ' % hostname, end='')
+            sys.stdout.flush()
+            child.expect(COMMAND_PROMPT)
+            child.sendline('3')
+            child.expect(COMMAND_PROMPT)
+            child.sendline('6')
+            child.expect(COMMAND_PROMPT)
+            child.sendline('Y')
+            print('done')
 	else:
 	    # cycle specific outlet
+            print('Cycle load on outlet: %s' % cycle)
 	    send_command(child, '5')
 	    send_command(child, '1')
 	    send_command(child, cycle)
 	    send_command(child, '4')
-	send_command(child, 'Y')
+	    send_command(child, 'Y')
 
     elif power_on != None:
-	print('power_on: %s'%(power_on))
-	if power_on == 'all':
+	if 'all' in power_on:
 	    # power on all loads
-	    send_command(child, '3')
-	    send_command(child, '9')
+            print('Power on ALL loads on %s... ' % hostname, end='')
+            sys.stdout.flush()
+            child.expect(COMMAND_PROMPT)
+            child.sendline('3')
+            child.expect(COMMAND_PROMPT)
+            child.sendline('9')
+            child.expect(COMMAND_PROMPT)
+            child.sendline('Y')
+            print('done')
 	else:
 	    # power on specific outlet
             send_command(child, '5')
             send_command(child, '1')
             send_command(child, power_on)
             send_command(child, '3')
-	send_command(child, 'Y')
+	    send_command(child, 'Y')
 
     elif power_off != None:
-	print('power_off: %s'%(power_off))
-	if power_off == 'all':
+	if 'all' in power_off:
 	    # power off all loads
-	    send_command(child, '3')
-	    send_command(child, '5')
+            print('Power off ALL loads on %s... ' % hostname, end='')
+            sys.stdout.flush()
+            child.expect(COMMAND_PROMPT)
+            child.sendline('3')
+            child.expect(COMMAND_PROMPT)
+            child.sendline('5')
+            child.expect(COMMAND_PROMPT)
+            child.sendline('Y')
+            print('done')
 	else:
 	    # power off specific outlet
 	    send_command(child, '5')
             send_command(child, '1')
             send_command(child, power_off)
             send_command(child, '3')
-	send_command(child, 'Y')
+	    send_command(child, 'Y')
 
-    elif identity != None:
-	print('identity')
-	send_command(child, '2')
-
-    # Now exit the remote host.
-    send_command(child, 'M')
+    # Now exit the remote host and close the connection.
+    child.sendline('M')
+    child.expect(COMMAND_PROMPT)
     child.sendline ('Q')
-    child.expect(pexpect.EOF)
+    child.close()
 
 if __name__ == "__main__":
     main()
