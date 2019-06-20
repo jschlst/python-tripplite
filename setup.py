b80f3293c0e8ac0b002822ca1c59f9d0951f5690
Jay Schulist <jayschulist@gmail.com>
2019-06-19 23:19:19 -0500

    add package files

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='python-tripplite',
+    version='0.1.1',
+    description='Tripp Lite Power Distribution Unit (PDU) control using python script',
+    url='https://github.com/jschlst/python-tripplite',
+    author='Jay Schulist',
+    author_email='jayschulist@gmail.com',
+    py_modules=['tripplite'],
+    license='GPL-3.0'
+)
